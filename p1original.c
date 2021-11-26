00b662372ac487edbbbbd6cd768ed109a73604f4
12345114 <87762102+12345114@users.noreply.github.com>
2021-11-26 03:56:30 +0000

    sum of two num

--- p1original.c
+++ p1original.c
@@ -1,2 +1,12 @@
-
+#include<stdio.h>
+int main(void)
+{
+  int a,b,sum;
+  printf("enter the value of a\n");
+  scanf("%d",&a);
+  printf("enter the value of b\n");
+  scanf("%d",&b);
+  sum=a+b;
+  printf("sum of numbers %d",sum);
+  return 0;
 }
